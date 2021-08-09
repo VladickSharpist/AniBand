7570b6e8ef72762a016d93973e3b7d44db05cf1a
Vlad <vlad.eroshenko.96@mail.ru>
2021-08-09 22:18:32 +0300

    Save provided file loggers

--- AniBand.Core/Infrastructure/Logger/FileLogger/FileLoggerProvider.cs
+++ AniBand.Core/Infrastructure/Logger/FileLogger/FileLoggerProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
 namespace AniBand.Core.Infrastructure.Logger.FileLogger
@@ -6,14 +7,17 @@ namespace AniBand.Core.Infrastructure.Logger.FileLogger
     public class FileLoggerProvider : ILoggerProvider
     {
         private string _path;
+        private List<FileLogger> _loggers;
 
         public FileLoggerProvider(string path)
         {
             _path = path;
+            _loggers = new List<FileLogger>();
         }
 
         private void Dispose(bool disposing)
         {
+            _loggers.Clear();
         }
         
         public void Dispose()
@@ -24,6 +28,8 @@ namespace AniBand.Core.Infrastructure.Logger.FileLogger
 
         public ILogger CreateLogger(string categoryName)
         {
+            var fileLogger = new FileLogger(_path);
+            _loggers.Add(fileLogger);
             return new FileLogger(_path);
         }
     }
